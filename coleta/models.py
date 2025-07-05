a4d33908ce4e5098da2554122f3dfbfb147f5d32
Yumiko Atae <dev.atae02@gmail.com>
2025-07-05 15:44:15 -0300

    feat: add latitude and longitude with auto-geocoding to PostoDeColeta

--- coleta/models.py
+++ coleta/models.py
@@ -1,10 +1,27 @@
 from django.db import models
+from geopy.geocoders import Nominatim
+from django.core.exceptions import ValidationError
 
 class PostoDeColeta(models.Model):
     nome = models.CharField(max_length=100)
     endereco = models.CharField(max_length=200)
     horario_funcionamento = models.CharField(max_length=100)
     feedback = models.TextField(blank=True)
+
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Só tenta buscar se ainda não tem coordenadas
+        if (not self.latitude or not self.longitude) and self.endereco:
+            geolocator = Nominatim(user_agent="reciclatech")
+            location = geolocator.geocode(self.endereco)
+            if location:
+                self.latitude = location.latitude
+                self.longitude = location.longitude
+            else:
+                raise ValidationError("Endereço não encontrado para geocodificação.")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nome
