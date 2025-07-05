dd69da6051211e086995613407739fc84ccd55a2
Yumiko Atae <dev.atae02@gmail.com>
2025-07-05 17:00:29 -0300

    feat: adiciona API REST para postos de coleta com DRF

--- coleta/urls.py
+++ coleta/urls.py
@@ -1,13 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from coleta.views import PostoDeColetaViewSet
+from .views import PostoDeColetaViewSet
 
 router = DefaultRouter()
 router.register(r'postos', PostoDeColetaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(coleta.urls)),
+    path('', include(router.urls)),
 ]
 
